d9f13083a1fd1f9add01974d172a949cd2d66914
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2021-05-02 19:38:03 +0200

    Angular project, all necessary end-points called
    Using Angular, built the client-side for an application called Movietron based on
    its existing server-side code (REST API and database), with supporting
    documentation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//  HttpClientModule is a simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side
+import { HttpClientModule } from '@angular/common/http';
+
+// after import add HttpClientModule here so you can use the Angular HttpClient module in your application
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
